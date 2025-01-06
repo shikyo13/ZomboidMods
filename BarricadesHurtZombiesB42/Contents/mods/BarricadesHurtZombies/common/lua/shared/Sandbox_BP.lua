e5e2636a21a6b6ee2ab2d7cdde58db559ecf9f6d
ZeroTheAbsolute <shikyo13@gmail.com>
2025-01-06 13:11:06 -0600

    Actual updates applied, new preview thumbnail created.

---
+++ BarricadesHurtZombiesB42/Contents/mods/BarricadesHurtZombies/common/lua/shared/Sandbox_BP.lua
@@ -0,0 +1,12 @@
+Sandbox_BP = {
+    BarricadesHurtZombies = {
+        BarricadeDamage = "Barricade Damage",
+        BarricadeDamage_tooltip = "Amount of damage zombies take when hitting barricades (as percentage of their health)",
+        
+        HurtingBarricade = "Damaging Barricades",
+        HurtingBarricade_tooltip = "Which types of barricades will damage zombies",
+        HurtingBarricade_option1 = "Player Built Only",
+        HurtingBarricade_option2 = "All Barricades",
+        HurtingBarricade_option3 = "None",
+    }
+}
